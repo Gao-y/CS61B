a7aa6e5bc4b4ab5796c866da08893b2049edfd57
Yuan Gao <gybrant96@gmail.com>
2019-09-16 21:20:04 -0700

    completed first part of lab1

---
+++ lab1/Numbers.java
@@ -0,0 +1,12 @@
+public class Numbers{
+	public static int sum3(int i,int j,int k){
+		return i+j+k;
+	}
+	public static void main(String[] args){
+		int x=0;
+		int y=3;
+		int z=4;
+
+		System.out.println(sum3(x,y,z));
+	}
+}
